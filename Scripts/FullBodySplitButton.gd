bbd18e7b1557c8a454c6bb058b897de0a4c1db90
ARIANA <ariana.kh21@gmail.com>
2025-01-05 16:07:22 +0330

    Vol/Split Selection

--- Scripts/FullBodySplitButton.gd
+++ Scripts/FullBodySplitButton.gd
@@ -11,6 +11,5 @@ func _process(delta):
 	pass
 
 
-
 func _on_pressed():
-	get_tree().change_scene_to_file("res://Scenes/VolSelection.tscn")
+	get_tree().change_scene_to_file("res://Scenes/WorkoutPlan.tscn")
