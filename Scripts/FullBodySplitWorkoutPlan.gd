947990f52d45654e4d446653914cd3764341b1d4
ARIANA <ariana.kh21@gmail.com>
2025-01-18 18:45:43 +0330

    PPL WP Script

--- Scripts/FullBodySplitWorkoutPlan.gd
+++ Scripts/FullBodySplitWorkoutPlan.gd
@@ -1,51 +1,80 @@
 extends Control
 
-#arrays for exercises
-var exercises = {
-	"1. ": ["Barbell squats", "Leg press", "Smith squats", "Leg extensions"],
-	"2. ": ["Barbell chest press", "Dumbbell chest press", "Push ups", "Chest press machine"],
-	"3. ": ["Lat pull downs", "Barbell rows", "Machine rows", "Single arm dumbbell row"],
-	"4. ": ["Dumbbell lunge", "Dumbbell deadlift", "Lying hamstring curl", "Dumbbell romanian deadlift"],
-	"5. ": ["Dumbbell shoulder press (seated)", "Shoulder press machine", "Dumbbell lateral raise", "Dumbbell reverse fly"],
-	"6. ": ["Dumbbell tricep kickbacks", "Tricep dips", "Cable tricep pushdown", "Over head tricep extension"],
-	"7. ": ["Bicep curls", "Barbell curl", "Concentration curls", "Cable bicep curls"],
-	"8. ": ["Plank", "Leg raise", "Mountain climbers", "Russian twists"]
+# Push exercises
+var push_exercises = {
+	"1. ": ["Front squat", "Leg press", "Smith Squats"],
+	"2. ": ["Barbell press", "Bench press machine"],
+	"3. ": ["Leg press", "DB Goblet squat"],
+	"4. ": ["Tricep dip", "Tricep rope pushdown"],
+	"5. ": ["Diamond push-up"],
+	"6. ": ["Standing calf raise", "Seated calf raise"],
+	"7. ": ["Standing cable crunch", "Mountain climbers"],
+	"8. ": ["Plank", "Leg raise"]
 }
 
-#arrays for sets and reps
-var sets_options = [3, 4]  
-var reps_options = [8, 10, 12, 15]  
+# Pull exercises
+var pull_exercises = {
+	"1. ": ["Leg extensions", "Lying leg curl", "Leg extension machine", "Lying hamstring curl"],
+	"2. ": ["DB Romanian deadlift", "BB Romanian deadlift"],
+	"3. ": ["Lat pull down", "Pull-up", "BB Row", "Seated row machine"],
+	"4. ": ["DB Bicep curl", "BB Bicep curl"],
+	"5. ": ["Concentration curls", "DB Hammer curls", "Cable bicep curls"],
+	"6. ": ["BB Forearm", "DB Forearm"],
+	"7. ": ["Kneeling cable crunch", "Crunch machine"],
+	"8. ": ["Reverse plank", "Side plank"]
+}
 
-#reference to the Label node
-@onready var workout_label = $Label  
+# Leg exercises
+var leg_exercises = {
+	"1. ": ["Deadlift", "Leg press", "Leg extension"],
+	"2. ": ["Squats", "Lunges", "Leg press"],
+	"3. ": ["Leg press", "DB Goblet squat", "Squats"],
+	"4. ": ["Seated leg curl", "Leg press", "Deadlift"],
+	"5. ": ["Calf raise", "Standing calf raise", "Seated calf raise"],
+	"6. ": ["Glute bridges", "Hip thrust", "Leg press"],
+	"7. ": ["Leg press", "Romanian deadlift", "Leg extensions"],
+	"8. ": ["Glute bridges", "Deadlift", "Seated leg curl"]
+}
+
+# Sets/reps
+var sets_options = [3, 4]
+var reps_options = [8, 10, 12, 15]
+
+@onready var workout_label = $Label
 
 func _ready():
+	randomize()  
 	generate_workout_plan()
 
-func generate_workout_plan():
+func generate_workout_plan() -> void:
 	var workout_plan = ""
-	
+
+	workout_plan += add_day_workout("Day 1:", push_exercises)
+	workout_plan += add_day_workout("Day 2:", pull_exercises)
+	workout_plan += add_day_workout("Day 3:", leg_exercises)
+
+	workout_label.text = workout_plan
+
+func add_day_workout(day_name: String, exercises: Dictionary) -> String:
+	var workout_day_plan = day_name + "\n\n"
+
 	for muscle_group in exercises.keys():
-		workout_plan += muscle_group 
+		workout_day_plan += muscle_group
 		
-		#Select random exercises
 		var selected_exercises = get_random_exercises(exercises[muscle_group], 1)
-		
+
 		for exercise in selected_exercises:
-			#select random sets and reps
 			var sets = select_random(sets_options)
 			var reps = select_random(reps_options)
-			workout_plan += str(exercise) + "      " + str(sets) + " x " + str(reps) + "\n"
-		workout_plan += "\n"
-	
-	workout_label.text = workout_plan  #display workout plan in the label
+			workout_day_plan += str(exercise) + "      " + str(sets) + " x " + str(reps) + "\n"
+		workout_day_plan += "\n"
 
-#select random exercises from a list
+	return workout_day_plan
+
 func get_random_exercises(exercise_list: Array, count: int) -> Array:
-	var shuffled_list = exercise_list.duplicate()  #duplicate the list to avoid modifying the original
-	shuffled_list.shuffle()  #Shuffle the duplicated list
-	return shuffled_list.slice(0, count)  #return the first 'count' exercises
+	var shuffled_list = exercise_list.duplicate()
+	shuffled_list.shuffle()
+	return shuffled_list.slice(0, count)
 
-#select random values from a list
 func select_random(options: Array) -> int:
 	return options[randi() % options.size()]
