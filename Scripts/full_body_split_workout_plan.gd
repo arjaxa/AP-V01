d24f43c7be57a728a9d6c23be559f88081e96c46
arjaxa <ariana.kh21@gmail.com>
2025-02-25 16:31:13 +0330

    ex1 edited

--- Scripts/full_body_split_workout_plan.gd
+++ Scripts/full_body_split_workout_plan.gd
@@ -1,7 +1,7 @@
 extends Control
 
 var exercises_full_body = {
-	"1. ": ["test"],# lat
+	"1. ": ["Lat pulldown", "alternative lat pulldown", "BB Row (reversed grip)", "BB Row", "Lat pulldown (Short grip)"],# lat
 	"2. ": ["Decline dumbbell fly", "Cable fly", "Dumbbell pullover", "Machine rows"],# lat
 	"3. ": ["Lat pull downs", "Barbell rows", "Machine rows", "Lat pull-down"],# lat/back
 	"4. ": ["Reverse dumbbell front raise", "Single arm dumbbell row", "DB Upright row", "DB Lateral raise"],# back
